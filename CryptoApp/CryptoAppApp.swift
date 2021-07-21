711e1561ec63ead29fe17d17e6ef7f0a59843e0d
Sergio Sepulveda <sergiosepulveda09@live.com>
2021-07-20 23:02:01 -0700

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Sergio Sepulveda on 2021-07-20.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
